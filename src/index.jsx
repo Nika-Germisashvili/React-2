86bc4b8878ced428e8de6f06bc713bbcd9181442
Nika-Germisashvili <127045155+Nika-Germisashvili@users.noreply.github.com>
2024-01-27 18:18:54 +0100

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+
+class App extends React.Component {
+    render() {
+        return (
+            <Header></Header>
+        )
+    }
+}
+
+var root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<App></App>);
